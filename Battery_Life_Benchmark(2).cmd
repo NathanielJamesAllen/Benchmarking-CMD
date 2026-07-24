53a09b14c958157a4d5d19265da740375a9e4d06
James Allen <nathanieljamesallen2@gmail.com>
2026-07-24 16:13:48 -0400

    "brightness settings"

--- Battery_Life_Benchmark(2).cmd
+++ Battery_Life_Benchmark(2).cmd
@@ -3,6 +3,9 @@ IF NOT EXIST "C:\ProcyonResults" mkdir "C:\ProcyonResults"
 cd c:\
 cd "C:\Program Files\UL\Procyon"
 
+echo Setting brightness to 100%
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Get-CimInstance -Namespace root/WMI -ClassName WmiMonitorBrightnessMethods | ForEach-Object { Invoke-CimMethod -InputObject $_ -MethodName WmiSetBrightness -Arguments @{Timeout=1;Brightness=100} }"
+
 start /WAIT "Running Procyon Battery Benchmark" .\ProcyonCmd.exe --definition=office_productivity_batterylife.def --systeminfo on --systeminfomonitor on --export-pdf C:\ProcyonResults\myBatteryResult2.procyon-result --log C:\Temp\ProcyonLogs\procyon.log
 
 pause
